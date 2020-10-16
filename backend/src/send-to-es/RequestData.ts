17e0f808a8492e137f74fe7b5081812b4f195e97
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-10-16 08:50:15 +0200

    chore: update request data

--- backend/src/send-to-es/RequestData.ts
+++ backend/src/send-to-es/RequestData.ts
@@ -1,11 +1,11 @@
 export class RequestData {
-    url: string; // Previously `id`
-    title: string // Previously `name`
-    discipline: string;
-    intendedEndUserRole: string;
-    educationalContext: string;
-    learningResourceType: string;
-    sourceContentType: string;
-    toolCategory: string;
-    objectType: string; // MATERIAL / TOOL / SOURCE / LESSONPLANNING
+  url: string; // Previously `id`
+  title: string; // Previously `name`
+  discipline: string; // Fachgebiet
+  intendedEndUserRole: string; // Zielgruppe
+  educationalContext: string; // Bildungsumfeld
+  learningResourceType: string; // Materialart (LOM)
+  sourceContentType: string; // Art der Seite -- Für Quellen
+  toolCategory: string; // Art des Tools -- Für Tools/Werkzeuge
+  objectType: 'MATERIAL' | 'TOOL' | 'SOURCE' | 'LESSONPLANNING'; // Typ des Objekts
 }
